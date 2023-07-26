6e5da2f0bf1a91909fda4e36922c5048367f6d49
MCG2603 <miraccangursoy1@gmail.com>
2023-07-26 17:38:28 +0300

    java4

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x,int y){
+        this.x=x;
+        this.y=y;
+
+    }
+    public double distance(){
+        return Math.sqrt((this.x*this.x)+(this.y*this.y));
+    }
+    public double distance(Point p){
+        return Math.sqrt(((this.x-p.x)*(this.x-p.x))+((this.y-p.y)*(this.y-p.y)));
+    }
+    public double distance(int a,int b){
+        return Math.sqrt(((this.x-a)*(this.x-a))+((this.y-b)*(this.y-b)));
+    }
 }
